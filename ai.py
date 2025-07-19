e4b5775d45bdd0e029f1517ff3bc4590c1a0ca1b
santilafguz <santilafguz@gmail.com>
2025-07-18 23:23:54 -0500

    VS code edits

---
+++ ai.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from openai import OpenAI, APIError
+
+try:
+    from google.colab import userdata
+    NRP_API_KEY = userdata.get('NRP_API_KEY')
+except (ImportError, ModuleNotFoundError):
+    NRP_API_KEY = os.environ.get("NRP_API_KEY")
+    if not NRP_API_KEY:
+        raise RuntimeError("API Key not found. Please set the NRP_API_KEY environment variable or run in Google Colab with secrets.")
+except userdata.SecretNotFoundError:
+    raise RuntimeError("API Key not found. Please add it to Colab Secrets as 'NRP_API_KEY'.")
+
+BASE_URL = "https:llm.nrp-nautilus.io/v1"
+client = OpenAI(
+    api_key=NRP_API_KEY,
+    base_url=BASE_URL
+)
+
+def Retrieve_models():
+  ml = []
+  models = client.models.list()
+  for model in models.data:
+    ml.append(f"{model.id}")
+
+  sort_models = sorted(ml)
+
+  for x in sort_models:
+    print(x)
+
+
+Retrieve_models()
